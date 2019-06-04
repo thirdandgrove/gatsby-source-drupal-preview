825d0817c523584ba71dfb36dd9fdfbd0a4fb506
Grant Glidewell <grant@thirdandgrove.com>
2019-06-04 16:09:15 -0700

    added relationship resolution to node uppdates

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -246,16 +246,16 @@ exports.sourceNodes = async (
       node.relationships = {};
       // handle relationships
       if (nodeToUpdate.relationships) {
-        _.each(nodeToUpdate.relationships, (v, k) => {
-          if (!v.data) return;
-          if (_.isArray(v.data) && v.data.length > 0) {
-            v.data.forEach(data => addBackRef(data.id, nodeToUpdate));
-            node.relationships[`${k}___NODE`] = _.compact(
-              v.data.map(data => (ids[data.id] ? createNodeId(data.id) : null))
+        _.each(nodeToUpdate.relationships, (value, key) => {
+          if (!value.data) return;
+          if (_.isArray(value.data) && value.data.length > 0) {
+            value.data.forEach(data => addBackRef(data.id, nodeToUpdate));
+            node.relationships[`${key}___NODE`] = _.compact(
+              value.data.map(data => createNodeId(data.id))
             );
-          } else if (ids[v.data.id]) {
-            addBackRef(v.data.id, nodeToUpdate);
-            node.relationships[`${k}___NODE`] = createNodeId(v.data.id);
+          } else if (ids[value.data.id]) {
+            addBackRef(value.data.id, nodeToUpdate);
+            node.relationships[`${key}___NODE`] = createNodeId(value.data.id);
           }
         });
       }
